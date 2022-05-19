0a5b532dced60e2aaba90c3229a7056153c2eefd
JarredTheCoder <105827461+JarredTheCoder@users.noreply.github.com>
2022-05-19 18:19:35 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,95 @@
-var color; function colourRandom() { var num = Math.floor(Math.random() * Math.pow(2, 24)); return '#' + ('00000' + num.toString(16)).substr(-6); } ext._shutdown = function() {};ext._getStatus = function() { return {status: 2, msg: 'Ready'};}; ext.color = function (callback){ color = colourRandom(); }; ext.say = function (a,callback){ alert("Hello!") }; var descriptor = { blocks: [ ["r","( %c)","color"],[" ","( hello)","say"],],menus: {},url: 'https://kyleplo.github.io/scratch-extension-creator/help#'Hello!};ScratchExtensions.register('JavaScript Scratch Edition', descriptor, ext);
+// We use class syntax to define our extension object
+
+// This isn't actually necessary, but it tends to look the best
+
+class JavaScriptScratch {
+
+  /**
+
+   * Scratch will call this method *once* when the extension loads.
+
+   * This method's job is to tell Scratch things like the extension's ID, name, and what blocks it supports.
+
+   */
+
+  getInfo() {
+
+    return {
+
+      // `id` is the internal ID of the extension
+
+      // It should never change!
+
+      // If you choose to make an actual extension, please change this to something else.
+
+      // Only the characters a-z and 0-9 can be used. No spaces or special characters.
+
+      id: 'jscratch',
+
+      // `name` is what the user sees in the toolbox
+
+      // It can be changed without breaking projects.
+
+      name: 'Javascript',
+
+      blocks: [
+
+        {
+
+          // `opcode` is the internal ID of the block
+
+          // It should never change!
+
+          // It corresponds to the class method with the same name.
+
+          opcode: 'sayfromjavascript',
+
+          blockType: Scratch.BlockType.Stack,
+
+          text: 'Hello!',
+          
+          arguments: {
+
+            ONE: {
+
+              type: Scratch.ArgumentType.STRING,
+
+              defaultValue: ''
+
+            }
+
+        }
+
+      ]
+
+    };
+
+  }
+
+  /**
+
+   * Corresponds to `opcode: 'hello'` above
+
+   */
+
+  sayfromjavascript() {
+
+    // You can just return a value: any string, boolean, or number will work.
+
+    // If you have to perform an asynchronous action like a request, just return a Promise.
+
+    // The block will wait until the Promise resolves and return the resolved value.
+    
+
+
+    return alert(args.ON);
+
+  }
+
+}
+
+// Call Scratch.extensions.register to register your extension
+
+// Make sure to register each extension exactly once
+
+Scratch.extensions.register(new JavaScriptScratch());
